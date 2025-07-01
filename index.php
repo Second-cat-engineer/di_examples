d6a53125abef38b2381fef25416cd529f70cb9ae
Yaylyaev Safuan <yaylyaevsafuan@gmail.com>
2025-07-01 22:29:24 +0300

    use di container symfony (yml)

--- index.php
+++ index.php
@@ -2,20 +2,29 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use App\Calculator\SimpleCalculator;
 use App\Cart;
 use App\CartItem;
-use App\Storage\SimpleStorage;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 $items = [
     new CartItem(1, 5, 100),
     new CartItem(2, 10, 50),
-    new CartItem(3, 3, 13)
+    new CartItem(3, 34, 13)
 ];
 $_SESSION['cart'] = serialize($items);
 
+//___________
+$container = new ContainerBuilder();
+$loader = new YamlFileLoader(
+    $container,
+    new Symfony\Component\Config\FileLocator(__DIR__)
+);
+$loader->load('services.yml');
+//___________
+
 try {
-    $cart = new Cart(new SimpleCalculator(), new SimpleStorage('cart'));
+    $cart = $container->get(Cart::class);
     echo $cart->getCost() . PHP_EOL;
 
 } catch (Exception $e) {
