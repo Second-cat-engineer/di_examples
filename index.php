c352faab9adefe12da69b90ef189bb86dfc18ea6
Yaylyaev Safuan <yaylyaevsafuan@gmail.com>
2025-07-01 22:41:09 +0300

    use di container yii3

--- index.php
+++ index.php
@@ -2,20 +2,44 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use App\Calculator\CalculatorInterface;
 use App\Calculator\SimpleCalculator;
 use App\Cart;
 use App\CartItem;
 use App\Storage\SimpleStorage;
+use App\Storage\StorageInterface;
+use Yiisoft\Definitions\Reference;
+use Yiisoft\Di\Container;
+use Yiisoft\Di\ContainerConfig;
 
 $items = [
     new CartItem(1, 5, 100),
     new CartItem(2, 10, 50),
-    new CartItem(3, 3, 13)
+    new CartItem(3, 34, 13)
 ];
 $_SESSION['cart'] = serialize($items);
 
+//___________
+$config = ContainerConfig::create()
+    ->withDefinitions([
+        StorageInterface::class => [
+            'class' => SimpleStorage::class,
+            '__construct()' => ['cart'],
+        ],
+        CalculatorInterface::class => SimpleCalculator::class,
+        Cart::class => [
+            '__construct()' => [
+                Reference::to(CalculatorInterface::class),
+                Reference::to(StorageInterface::class),
+            ],
+        ],
+    ]);
+
+$container = new Container($config);
+//___________
+
 try {
-    $cart = new Cart(new SimpleCalculator(), new SimpleStorage('cart'));
+    $cart = $container->get(Cart::class);
     echo $cart->getCost() . PHP_EOL;
 
 } catch (Exception $e) {
