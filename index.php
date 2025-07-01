5b3296eb5b82c4037bd7fe97178714012ba61958
Yaylyaev Safuan <yaylyaevsafuan@gmail.com>
2025-07-01 22:25:56 +0300

    use di container symfony (php)

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use App\Calculator\CalculatorInterface;
 use App\Calculator\SimpleCalculator;
 use App\Cart;
 use App\CartItem;
 use App\Storage\SimpleStorage;
+use App\Storage\StorageInterface;
+use Symfony\Component\DependencyInjection\Reference;
 
 $items = [
     new CartItem(1, 5, 100),
@@ -14,8 +17,23 @@ $items = [
 ];
 $_SESSION['cart'] = serialize($items);
 
+//___________
+$container = new Symfony\Component\DependencyInjection\ContainerBuilder();
+$container->setParameter('cart_store', 'cart');
+
+$container->register(StorageInterface::class, SimpleStorage::class)
+    ->addArgument('%cart_store%');
+
+$container->register(CalculatorInterface::class, SimpleCalculator::class);
+
+$container->register(Cart::class, Cart::class)
+    ->addArgument(new Reference(CalculatorInterface::class))
+    ->addArgument(new Reference(StorageInterface::class))
+    ->setShared(true);
+//___________
+
 try {
-    $cart = new Cart(new SimpleCalculator(), new SimpleStorage('cart'));
+    $cart = $container->get(Cart::class);
     echo $cart->getCost() . PHP_EOL;
 
 } catch (Exception $e) {
