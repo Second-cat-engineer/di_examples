0e7b95146debbc5a8279605c679a10e162c96766
Yaylyaev Safuan <yaylyaevsafuan@gmail.com>
2025-07-01 22:38:25 +0300

    use di container laravel (php)

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use App\Calculator\CalculatorInterface;
 use App\Calculator\SimpleCalculator;
 use App\Cart;
 use App\CartItem;
 use App\Storage\SimpleStorage;
+use App\Storage\StorageInterface;
+use Illuminate\Container\Container;
 
 $items = [
     new CartItem(1, 5, 100),
@@ -14,8 +17,19 @@ $items = [
 ];
 $_SESSION['cart'] = serialize($items);
 
+//___________
+$container = new Container();
+
+$container->bind(StorageInterface::class, fn () => new SimpleStorage('cart'));
+$container->bind(CalculatorInterface::class, fn () => new SimpleCalculator());
+$container->singleton(Cart::class, fn ($container) => new Cart(
+    $container->make(CalculatorInterface::class),
+    $container->make(StorageInterface::class)
+));
+//___________
+
 try {
-    $cart = new Cart(new SimpleCalculator(), new SimpleStorage('cart'));
+    $cart = $container->get(Cart::class);
     echo $cart->getCost() . PHP_EOL;
 
 } catch (Exception $e) {
