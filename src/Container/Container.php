1b4b842ffdd550c59de8ab10013b4e914cdace6c
Yaylyaev Safuan <yaylyaevsafuan@gmail.com>
2025-07-01 21:59:13 +0300

    add step 5

--- src/Container/Container.php
+++ src/Container/Container.php
@@ -3,6 +3,7 @@
 namespace App\Container;
 
 use Psr\Container\ContainerInterface;
+use ReflectionClass;
 
 class Container implements ContainerInterface
 {
@@ -18,11 +19,41 @@ class Container implements ContainerInterface
         if (!$this->has($id)) {
             throw new ServiceNotFoundException('Undefined service: ' . $id);
         }
-        return call_user_func($this->definitions[$id], $this);
+
+        $definition = $this->definitions[$id];
+
+        $component = is_string($definition)
+            ? $this->make($definition)
+            : $definition($this);
+
+        if (!$component) {
+            throw new ContainerException('Undefined component ' . $id);
+        }
+
+        return $component;
     }
 
     public function has(string $id): bool
     {
         return isset($this->definitions[$id]);
+    }
+
+    private function make(string $definition): ?object
+    {
+        if (!class_exists($definition)) {
+            return null;
+        }
+
+        $reflection = new ReflectionClass($definition);
+        $arguments = [];
+        if (($constructor = $reflection->getConstructor()) !== null) {
+            foreach ($constructor->getParameters() as $param) {
+                $paramClass = $param->getType();
+
+                $arguments[] = $paramClass ? $this->get($paramClass->getName()) : null;
+            }
+        }
+
+        return $reflection->newInstanceArgs($arguments);
     }
 }
